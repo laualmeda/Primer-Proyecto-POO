698edcce39725585b301036c126d4f34833af985
Isa-Borges <157188813+Isa-Borges@users.noreply.github.com>
2024-04-26 15:43:05 -1100

    Isabela_Math -ecuacion.java

--- Math -ecuacion.java
+++ Math -ecuacion.java
@@ -4,5 +4,13 @@ public class MathEquation {
         double y = calculateEquation(x);
         
     System.out.println("El valor de y es: " + y);
+    
     }
+    public static double calculateEquation(double x) {
+        double raiz = Math.sqrt(Math.pow(x, 2) + 1);
+        double logaritmo = Math.log(x);
+        double integral = integrate(x, 3, 2);
+        double derivada = derivative(x, 4, 3);
+        double resultado = (raiz + logaritmo) / (integral + derivada);
+        return resultado;
 }
